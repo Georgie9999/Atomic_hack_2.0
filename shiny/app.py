81f535b94b30967742d4d3d65e1b9924e72277e5
Георгий <sazykin.ga@gmail.com>
2024-06-15 13:11:48 +0300

    test shiny commit version 2

--- shiny/app.py
+++ shiny/app.py
@@ -1,4 +1,4 @@
-import plotly.express as px
+
 from shiny.express import input, render, ui
 from shinywidgets import render_widget
 from shiny.types import ImgData
@@ -19,16 +19,13 @@ with ui.layout_columns(col_widths=[12, 12, 12]):
     with ui.layout_column_wrap(fill=False):
         with ui.value_box():
             "Размер изображений(Мб)"
-            "$1,000,000"
+            ""
         with ui.value_box():
             "Разрешение"
-            "$1,000,000"
+            ""
     with ui.card(height=400):
         ui.card_header("Итоговый результат:")
     with ui.card(height=400):
         ui.card_header("Распределение вероятностей по дефекту:")
 
 ui.page_opts(title="Определение дефектов сварных швов с помощь ИИ", window_title="sbs_solution")
-
-with ui.sidebar():
-    ui.input_select("var", "Выберите один из вариантов:", choices=["total_bill", "tip"])
